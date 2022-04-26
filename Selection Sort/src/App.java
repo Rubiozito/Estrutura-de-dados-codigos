20246dfc6b411f35113fb96276996edfe0862f03
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-04-26 08:00:35 -0300

    ok

--- Selection Sort/src/App.java
+++ Selection Sort/src/App.java
@@ -1,13 +1,9 @@
 public class App {
-    public static void selections(int[] lista)
-    {
-        for (int i = 0; i < lista.length; i++)
-        {
+    public static void selections(int[] lista){
+        for (int i = 0; i < lista.length; i++){
             int index = i;
-            for(int j = i+1; j < lista.length; j++)
-            {
-                if(lista[j] < lista[index])
-                {
+            for(int j = i+1; j < lista.length; j++){
+                if(lista[j] < lista[index]){
                     index = j;
                 }
             }
@@ -16,11 +12,11 @@ public class App {
             lista[i] = menor;
         }
     }
+
     public static void main(String[] args) throws Exception {
         int[] lista = {10, 5, 20, 4, 8, 12, 25, 1};
         System.out.println("Lista antes da ordenacao:");
-        for(int i:lista)
-        {
+        for(int i:lista){
             System.out.print(i + " ");
         }
         System.out.println();
@@ -28,9 +24,9 @@ public class App {
         selections(lista);
 
         System.out.println("Lista ordenada:");
-        for(int i:lista)
-        {
+        for(int i:lista){
             System.out.print(i+" ");
         }
     }
+
 }
